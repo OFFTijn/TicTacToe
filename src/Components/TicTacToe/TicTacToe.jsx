1712fd92f445381fe6ce8a769a8cc950b628450b
OFFTijn <tijnm06@gmail.com>
2024-05-31 19:24:37 +0200

    Basis ReactJS +bootstrap

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TicTacToe.css';
+import circle_icon from '../Assets/o.png'
+import cross_icon from '../Assets/x.png'
+
+const TicTacToe = () => {
+    return (
+        <div className='container'>
+            <hi className="title">Tic Tac Toe Game</hi>
+            <div className="board">
+                
+            </div>
+            
+        </div>
+    );
+};
+
+export default TicTacToe;
